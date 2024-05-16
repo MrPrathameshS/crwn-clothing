01fb13bb44a9b263d5554e62d9b5877d374bea91
Prathamesh Shetye <prathameshshetye23@gmail.com>
2024-05-16 23:45:29 +0530

    Deploying to netlify

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyA9_UMCHqLvigQ8mEwdnNAj54pS7e3X0u8",
+  authDomain: "crwn-clothing-db-6158a.firebaseapp.com",
+  projectId: "crwn-clothing-db-6158a",
+  storageBucket: "crwn-clothing-db-6158a.appspot.com",
+  messagingSenderId: "1091986807760",
+  appId: "1:1091986807760:web:684e0ab2cfdc4cd065b7d5"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
